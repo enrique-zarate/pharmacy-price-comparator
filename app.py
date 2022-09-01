ace3cdf3342d257fd3767341de2058c8d9f52f0b
enrique-zarate <eszg.fury@gmail.com>
2022-09-01 15:44:18 -0400

    se agrega index

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "<h1>Hello, World!</h1>"
+    return render_template('index.html')
